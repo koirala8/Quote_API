fdde35f237194166c5e820cff4e2f0e7458c56dc
Mahendra Koirala <60951574+koirala8@users.noreply.github.com>
2023-12-13 19:24:48 -0600

    Created GET API Endpoints uisng EXPRESS server

--- server.js
+++ server.js
@@ -8,3 +8,36 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+app.get('/api/quotes/random', (req, res, next) => {
+    res.json({quote: getRandomElement(quotes)});
+});
+
+app.get('/api/quotes', (req, res, next) => {
+    const queryParams = req.query;
+
+    if (Object.keys(queryParams).length === 0) {
+        // If no query parameters are provided, return all quotes
+        res.json({ quotes: quotes });
+    } else {
+        const queryParamsPerson = queryParams.person;
+
+        if (queryParamsPerson) {
+            // Filter quotes based on the specified person
+            const personQuotes = quotes.filter(quote => quote.person === queryParamsPerson);
+
+            if (personQuotes.length > 0) {
+                res.json({ quotes: personQuotes });
+            } else {
+                res.json({ message: 'No quotes found for the specified person.' });
+            }
+        } else {
+            // If other query parameters are provided, handle them accordingly
+            res.json({ message: 'Unsupported query parameters.' });
+        }
+    }
+});
+
+
+
+
+app.listen(PORT);
